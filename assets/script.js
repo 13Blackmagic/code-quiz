5dfd59bb4a09add666844ce530760678b040775b
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-03-16 22:01:13 -0600

    fixed to center and color

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,25 @@ var question = "how much wood would a woodchuck chuck if a woodchuck could chuck
 var answer = "a lot";
 var message = "The answer to the question '" + question + "' is " + answer + ".";
 console.log(message);
+var question = "What is the airspeed velocity of an unladen swallow?";
+var answer = "African or European?";
+var message = "The answer to the question '" + question + "' is " + answer + ".";
+console.log(message);
+
+
+
+var prompt= require('prompt-sync')();
+var question = prompt('What is the answer to the Ultimate Question of Life, the Universe, and Everything?');
+var answer = 42;
+var message = "The answer to the question '" + question + "' is " + answer + ".";
+console.log(message);
+var question = prompt('how much wood would a woodchuck chuck if a woodchuck could chuck wood?');
+var answer = "a lot";
+var message = "The answer to the question '" + question + "' is " + answer + ".";
+console.log(message);
+var question = prompt('What is the airspeed velocity of an unladen swallow?');
+var answer = "African or European?";
+
 
 
 
