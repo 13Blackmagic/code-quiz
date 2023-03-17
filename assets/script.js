e8d19ab4154cd1df58d8438df763c71ab44d76b2
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-03-16 21:06:40 -0600

    added to html and script

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+var question = "What is the answer to the Ultimate Question of Life, the Universe, and Everything?";
+var answer = 42;
+var message = "The answer to the question '" + question + "' is " + answer + ".";
+console.log(message);
+var question = "how much wood would a woodchuck chuck if a woodchuck could chuck wood?";
+var answer = "a lot";
+var message = "The answer to the question '" + question + "' is " + answer + ".";
+console.log(message);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+generateBtn.addEventListener("click", answer-question);
