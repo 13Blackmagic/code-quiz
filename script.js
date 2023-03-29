1cb5fd4a859a445d2eaf1fc66d4ddbb54f97ce4e
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-03-28 21:51:20 -0600

    added if statements

--- script.js
+++ script.js
@@ -1,26 +1,63 @@
+var startBtn = document.querySelector("#start");
+var mainEl = document.querySelector("#main");
+var timeEl = document.querySelector("#time");
+startBtn.addEventListener("click", function() {
+  var question = showMessage("What is the answer to the Ultimate Question of Life, the Universe, and Everything?");
+  if (question === "42") {
+    alert("Correct!");
+  } else {
+    alert("Wrong!");
+  }
+  var question = showMessage("How much wood would a woodchuck chuck if a woodchuck could chuck wood?");
+  if (question === "a lot") {
+    alert("Correct!");
+  } else {
+    alert("Wrong!");
+  }
+  var question = showMessage("What is the airspeed velocity of an unladen swallow?");
+  if (question === "African or European?") {
+    alert("Correct!");
+  } else {
+    alert("Wrong!");
+  }
+  var question = showMessage("Who is the best student in the class?");
+  if (question === "I am") {
+    alert("Correct!");
+  } else {
+    alert("Wrong!");
+  }
+  showAnswer();
+  
+});
+function question () {
+  var question = timerInterval;
+  var answer = 42;
+  var message = "The answer to the question '" + question + "' is " + answer + ".";
+  console.log(message);
+}
 console.log(message);
-var question = prompt('What is the answer to the Ultimate Question of Life, the Universe, and Everything?');
+var question = ('What is the answer to the Ultimate Question of Life, the Universe, and Everything?');
 var answer = 42;
 var message = "The answer to the question '" + question + "' is " + answer + ".";
 console.log(message);
-var question = prompt('how much wood would a woodchuck chuck if a woodchuck could chuck wood?');
+var question = ('how much wood would a woodchuck chuck if a woodchuck could chuck wood?');
 var answer = "a lot";
 var message = "The answer to the question '" + question + "' is " + answer + ".";
 console.log(message);
-var question = prompt('What is the airspeed velocity of an unladen swallow?');
+var question = ('What is the airspeed velocity of an unladen swallow?');
 var answer = "African or European?";
 var message = "The answer to the question '" + question + "' is " + answer + ".";
 console.log(message);
-var question = prompt('Who is the best student in the class?');
+var question = ('Who is the best student in the class?');
 var answer = "I am";
 var message = "The answer to the question '" + question + "' is " + answer + ".";
 
-submitEl.addEventListener("click", showResponse);
-
-var timeEl = document.querySelector(".time");
 
 // Selects element by id
-var mainEl = document.getElementById("main");
+function showMessage(message) {
+  var question = prompt(message);
+  return question;
+}
 
 var secondsLeft = 10;
 
@@ -28,20 +65,18 @@ function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
 
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
       // Calls function to create and append image
-      sendMessage();
     }
 
   }, 1000);
 }
 
 // Function to create and append colorsplosion image
-function sendMessage() {
+function showAnswer() {
   timeEl.textContent = " ";
   var imgEl = document.createElement("img");
   imgEl.setAttribute("src", "images/image_1.jpg");
@@ -50,29 +85,3 @@ function sendMessage() {
 }
 
 setTime();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
