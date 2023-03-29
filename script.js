3f3af84c37632ffe22776aaba3bf23187f3c5dbe
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-03-29 14:53:30 -0600

    questions and answers

--- script.js
+++ script.js
@@ -6,13 +6,59 @@ let timeleft = 120;
 let scoreEl = document.querySelector("#score");
 startBtn.addEventListener("click", startMyQuiz);
 
+let questions = [
+  {
+    question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    answers: ["<script name='xxx.js'>",  "<script href='xxx.js'>", "<script src='xxx.js'>"],
+    correctAnswer: "<script src='xxx.js'>"
+  },
+  {
+    question: "How do you write 'Hello World' in an alert box?",
+    answers: ["msg('Hello World')", "alert('Hello World')", "msgBox('Hello World')", "alertBox('Hello World')"],
+    correctAnswer: "alert('Hello World')"
+  },
+  {
+    question: "How do you create a function in JavaScript?",
+    answers: ["function myFunction()", "function = myFunction()", "function:myFunction()"],
+    correctAnswer: "function myFunction()"
+  },
+  {
+    question: "How do you call a function named 'myFunction'?",
+    answers: ["call myFunction()", "call function myFunction()", "myFunction()"],
+    correctAnswer: "myFunction()"
+  },
+  {
+    question: "How to write an IF statement in JavaScript?",
+    answers: ["if i = 5", "if i == 5 then", "if (i == 5)", "if i = 5 then"],
+    correctAnswer: "if (i == 5)"
+  },
+];
+ let index=0;
+ let currantQuestion = questions[index];
+ let questionEl = document.querySelector("#question");
+ let answersEl = document.querySelector("#answers");
+  let answerBtns = document.querySelectorAll(".answer-btn");
+  let answerBtn1 = document.querySelector("#answer1");
+  let answerBtn2 = document.querySelector("#answer2");
+  let answerBtn3 = document.querySelector("#answer3");
+  let questionsDiv = document.querySelector("#question-div");
+
 function startMyQuiz() {
 let codeQuiz = document.querySelector("#code-quiz");
 codeQuiz.style.display = "none";
 timerId = setInterval(countdown, 1000);
 scoreEl.textContent = "Time: " + timeleft;
-  
+ questionsDiv.removeAttribute("class");
+showQuestion(); 
 }
+
+function showQuestion() {
+  questionEl.textContent = currantQuestion.question;
+  answerBtn1.textContent = currantQuestion.answers[0];
+  answerBtn2.textContent = currantQuestion.answers[1];
+  answerBtn3.textContent = currantQuestion.answers[2];
+}
+
 
 function countdown() {
   if (timeleft <= 0) {
